eb069db3dae3738b61ff995e6fdab32d2eafc6df
koji.mizugai <koji.mizugai@dena.jp>
2014-07-17 09:53:12 +0900

    add DB::Schema config

--- lib/Cobunko/DB/Schema.pm
+++ lib/Cobunko/DB/Schema.pm
@@ -8,9 +8,9 @@ use Teng::Schema::Declare;
 base_row_class 'Cobunko::DB::Row';
 
 table {
-    name 'member';
-    pk 'id';
-    columns qw(id name);
+    name 'books';
+    pk 'user_id';
+    columns qw(user_id title isbn);
 };
 
 1;
